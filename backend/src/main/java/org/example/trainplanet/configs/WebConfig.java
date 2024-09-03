ae030530df25b5cc1cab0aa3c51e869676627b86
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-09-03 17:15:03 +0200

    add allowed origin

--- backend/src/main/java/org/example/trainplanet/configs/WebConfig.java
+++ backend/src/main/java/org/example/trainplanet/configs/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                .allowedOrigins("http://localhost:5173", "https://train-booking-api-tid5.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
     }
 }
